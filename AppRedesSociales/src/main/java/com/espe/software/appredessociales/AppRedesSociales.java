49d53baa5edd98a9c07938f2b0861625d05a51c6
Jostyn Sandoval <jnsandoval@espe.edu.ec>
2026-02-05 08:59:05 -0500

    APP REDES SOCIALES STRATEGY

---
+++ AppRedesSociales/src/main/java/com/espe/software/appredessociales/AppRedesSociales.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.espe.software.appredessociales;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author labesp
+ */
+public class AppRedesSociales {
+    private RedSocial estrategia;
+    
+    public void setEstrategia (RedSocial estrategia){
+        this.estrategia = estrategia;
+    }
+    
+    public void ejecutarConexion(){
+        estrategia.conectar();
+    }
+    
+    public static void main(String[] args) {
+        AppRedesSociales miApp = new AppRedesSociales();
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Cual red quiere conectarse?");
+        System.out.println("1. Tiktok  -  2.Instragram  -  3.Facebook");
+        int opcion = entrada.nextInt();
+        
+        if (opcion == 1){
+            miApp.setEstrategia(new Tiktok());
+        } else if (opcion == 2){
+            miApp.setEstrategia(new Instagram());
+        } else if (opcion == 3){
+            miApp.setEstrategia(new Facebook());
+        }
+
+        miApp.ejecutarConexion();
+    }
+}
---
+++ AppRedesSociales/src/main/java/com/espe/software/appredessociales/Tiktok.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.espe.software.appredessociales;
+
+/**
+ *
+ * @author labesp
+ */
+public class Tiktok implements RedSocial{
+
+    @Override
+    public void conectar() {
+        System.out.println("Se conecto a Tiktok");
+    }
+    
+}
